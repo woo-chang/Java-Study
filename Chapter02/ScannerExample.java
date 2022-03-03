03a55949259ded60f2e8d9f0faf4243deaaecf2d
최우창 <clllickme@naver.com>
2022-03-03 22:46:35 +0900

    feat : String & Scanner

---
+++ Chapter02/ScannerExample.java
@@ -0,0 +1,14 @@
+package Chapter02;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String result = sc.nextLine();
+
+    System.out.println(result);
+  }
+
+}
---
+++ Chapter02/StringExample.java
@@ -0,0 +1,19 @@
+package Chapter02;
+
+public class StringExample {
+
+  public static void main(String[] args) {
+    String name = "Ja" + "va";
+    // 문자열의 덧셈 연산은 문자열이 아닌 타입이 문자열로 변환 후 실행
+    String result = name + 8;
+
+    Integer i = 7;
+    // 빈 문자열과의 덧셈연산을 통해 간단한 형변환 가능
+    String changeType = i + "";
+    System.out.println(result);
+    System.out.println(i.getClass().getSimpleName());
+    System.out.println(changeType.getClass().getSimpleName());
+
+    System.out.printf("age:%d", i);
+  }
+}
