bdcf23977e1ff98f6d292cfe88fbc8a7505f09f4
최우창 <clllickme@naver.com>
2022-04-07 21:55:35 +0900

    feat : 자료구조(트리) - 삭제 구현

--- DataStructure/Tree/NodeManagement.java
+++ DataStructure/Tree/NodeManagement.java
@@ -2,11 +2,11 @@ package DataStructure.Tree;
 
 public class NodeManagement {
 
-  private Node head;
+  Node head;
 
   static class Node {
-    private Node left;
-    private Node right;
+    Node left;
+    Node right;
     int value;
 
     public Node(int data) {
@@ -62,6 +62,122 @@ public class NodeManagement {
       }
       return null;
     }
+  }
+
+  public boolean delete(int value) {
+    boolean searched = false;
+
+    Node currParentNode = this.head;
+    Node currNode = this.head;
+
+    // 코너 케이스1: Node가 하나도 없을 때
+    if (this.head == null) {
+      return false;
+    } else {
+      // 코너 케이스2: Node가 단지 하나만 있고, 해당 Node가 삭제할 Node 일 때
+      if (this.head.value == value && this.head.left == null && this.head.right == null) {
+        this.head = null;
+        return true;
+      }
+
+      while (currNode != null) {
+        if (currNode.value == value) {
+          searched = true;
+          break;
+        } else if (currNode.value > value) {
+          currParentNode = currNode;
+          currNode = currNode.left;
+        } else {
+          currParentNode = currNode;
+          currNode = currNode.right;
+        }
+      }
+
+      if (searched == false) {
+        return false;
+      }
+
+      // currNode는 해당 데이터를 가지고 있는 노드
+      // currParentNode는 해당 데이터를 가지고 있는 노드의 부모 노드
+      // CASE1: 삭제할 노드가 Leaf Node인 경우
+      if (currNode.left == null && currNode.right == null) {
+        if (value < currParentNode.value) {
+          currParentNode.left = null;
+          // 안써도 되지만 명시적으로 작성
+          currNode = null;
+        } else {
+          currParentNode.right = null;
+          // 안써도 되지만 명시적으로 작성
+          currNode = null;
+        }
+        return true;
+      } else if (currNode.left != null && currNode.right == null) { // CASE2: 삭제할 노드 왼쪽 자식 노드 존재
+        if (value > currParentNode.value) {
+          currParentNode.right = currNode.left;
+          currNode = null;
+        } else {
+          currParentNode.left = currNode.left;
+          currNode = null;
+        }
+        return true;
+      } else if (currNode.left == null && currNode.right != null) { // CASE3: 삭제할 노드 오른쪽 자식 노드 존재
+        if (value > currParentNode.value) {
+          currParentNode.right = currNode.right;
+          currNode = null;
+        } else {
+          currParentNode.left = currNode.right;
+          currNode = null;
+        }
+        return true;
+      } else { // CASE4: 삭제할 노드 자식 노드 2개 존재
+        // CASE4-1: 삭제할 노드가 부모 노드 왼쪽에 있을 때
+        if (value < currParentNode.value) {
+          Node changeNode = currNode.right;
+          Node changeParentNode = currNode.right;
+
+          while(changeNode.left != null) {
+            changeParentNode = changeNode;
+            changeNode = changeNode.left;
+          }
+
+          // CASE4-1-1: 바꾸는 노드의 오른쪽 자식이 존재할 때
+          if (changeNode.right != null) {
+            changeParentNode.left = changeNode.right;
+          } else { // CASE4-1-2: 바꾸는 노드의 오른쪽 자식이 존재하지 않을 때
+            changeParentNode.left = null;
+          }
+
+          currParentNode.left = changeNode;
+          changeNode.left = currNode.left;
+          changeNode.right = currNode.right;
+          currNode = null;
+        } else { // CASE4-1: 삭제할 노드가 부모 노드 오른쪽에 있을 때
+          Node changeNode = currNode.right;
+          Node changeParentNode = currNode.right;
+
+          while(changeNode.left != null) {
+            changeParentNode = changeNode;
+            changeNode = changeNode.left;
+          }
+
+          // CASE4-1-1: 바꾸는 노드의 오른쪽 자식이 존재할 때
+          if (changeNode.right != null) {
+            changeParentNode.left = changeNode.right;
+          } else { // CASE4-1-2: 바꾸는 노드의 오른쪽 자식이 존재하지 않을 때
+            changeParentNode.left = null;
+          }
+
+          currParentNode.right = changeNode;
+          changeNode.left = currNode.left;
+          changeNode.right = currNode.right;
+          currNode = null;
+        }
+
+        return true;
+      }
+    }
+
+
   }
 
 }
--- DataStructure/Tree/NodeManagementTest.java
+++ DataStructure/Tree/NodeManagementTest.java
@@ -11,6 +11,34 @@ public class NodeManagementTest {
 
     System.out.println(nodeManagement.search(1).value);
     System.out.println(nodeManagement.search(12));
+
+    System.out.println("====================================");
+
+    NodeManagement myTree = new NodeManagement();
+    myTree.insertNode(10);
+    myTree.insertNode(15);
+    myTree.insertNode(13);
+    myTree.insertNode(11);
+    myTree.insertNode(14);
+    myTree.insertNode(18);
+    myTree.insertNode(16);
+    myTree.insertNode(19);
+    myTree.insertNode(17);
+    myTree.insertNode(7);
+    myTree.insertNode(8);
+    myTree.insertNode(6);
+    System.out.println(myTree.delete(15));
+    System.out.println("HEAD: " + myTree.head.value);
+    System.out.println("HEAD LEFT: " + myTree.head.left.value);
+    System.out.println("HEAD LEFT LEFT: " + myTree.head.left.left.value);
+    System.out.println("HEAD LEFT RIGHT: " + myTree.head.left.right.value);
+
+    System.out.println("HEAD RIGHT: " + myTree.head.right.value);
+    System.out.println("HEAD RIGHT LEFT: " + myTree.head.right.left.value);
+    System.out.println("HEAD RIGHT RIGHT: " + myTree.head.right.right.value);
+
+    System.out.println("HEAD RIGHT RIGHT LEFT: " + myTree.head.right.right.left.value);
+    System.out.println("HEAD RIGHT RIGHT RIGHT: " + myTree.head.right.right.right.value);
   }
 
 }
