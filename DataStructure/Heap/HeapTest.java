0c5d1c3535faa12d1f56d1f4fabd8d52e0332f04
최우창 <clllickme@naver.com>
2022-04-07 22:37:06 +0900

    feat : 자료구조(힙) - 데이터 삽입

---
+++ DataStructure/Heap/HeapTest.java
@@ -0,0 +1,15 @@
+package DataStructure.Heap;
+
+public class HeapTest {
+
+  public static void main(String[] args) {
+    Heap heap = new Heap(15);
+    heap.insert(10);
+    heap.insert(8);
+    heap.insert(5);
+    heap.insert(4);
+    heap.insert(20);
+
+    System.out.println(heap.heapArray);
+  }
+}
