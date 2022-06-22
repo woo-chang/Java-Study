92fbbbaf2001fd79444f972257b076cbdb5126c4
woo-chang <clllickme@naver.com>
2022-06-22 17:19:46 +0900

    Convert Set/Array

---
+++ code/ConvertSettoArray.java
@@ -0,0 +1,24 @@
+package code;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ConvertSettoArray {
+
+  public static void main(String[] args) {
+    Set<Integer> set = new HashSet<>();
+    set.add(1);
+    set.add(2);
+    set.add(3);
+
+    Integer[] array = set.toArray(new Integer[set.size()]);
+    Stream.of(array).forEach(a -> System.out.println(a));
+
+    array[2] = 1;
+
+    Set<Integer> set1 = Stream.of(array).collect(Collectors.toSet());
+    System.out.println(set1);
+  }
+}
